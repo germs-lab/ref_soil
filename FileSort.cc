ae2804af2773eef340b2a718218cb3c25b0c4bbb
Jin Choi <jinchoi721@gmail.com>
2015-04-03 19:32:45 -0500

    update

--- FileSort.cc
+++ FileSort.cc
@@ -1,6 +1,7 @@
 // usage:  g++ FileSort.cc -o FileSort
 // ./FileSort fileinput directory case1 case2
 // ./FileSort RefSoilListJinUnix.csv ../RefSoil16s case1 case2
+// ./FileSort RefSoilListJinUnix.csv genbank-files case1 case2
 #include <stdio.h>
 #include <iostream>
 #include <string>
@@ -10,137 +11,131 @@
 #include <vector>
 #include <sstream>
 
-#define maxFilename 40
-
 using namespace std;
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
 int main(int argc, char *argv[])
 {
-int OpenFile(FILE **filePoint, string fileInput);
-void CloseFile(FILE **filePoint);
-void PrintFileContents(FILE **filePoint);
-size_t getFilesize(const std::string& filename); 
+size_t getFilesize(const std::string& filename);
+int checkFile(ifstream &input); 
+string searchSize(string &st,vector <vector <string> > &da);
+void printMatrix(vector <vector <string> > &dad);
+void checkDir (string &direc);
 
+
+//arguments
 string fileInput = argv[1];
-string dirname= argv[2];
-//case 1 don't need wokr
+string dirname = argv[2];
+//case 1 don't need work
 string newdirname1= argv[3];
 //case 2 need HMM
 string newdirname2= argv[4];
 
-FILE *sourcefp;
+//*********************************************************
+//read full table of RefSoil list and put into matrix
+//*********************************************************
+//file for input
+ifstream inputTable;
+string table;
+inputTable.open(fileInput);
+vector <vector <string> > data;
+checkFile(inputTable);
 
-OpenFile(&sourcefp,fileInput);
+while(getline(inputTable,table))
+{
+	istringstream ss (table);
+	//this add data into the column (second number)
+	vector <string> record;
+		while (ss)
+		{
+			string s1;
+			if(!getline(ss,s1,',')) break;
+			if(s1!=""){
+				record.push_back(s1);
+			}
+		}
+		//this add data into the row (first number)
+		data.push_back(record);
 
-char info[100];
-string read;
-string delimiter = ",";
-string token;
-size_t pos = 0;
-int delflag=0;
-//open file
-fscanf(sourcefp,"%s",info);
-//read file
-typedef string MATRIX[1181][4];
-MATRIX list;
-MATRIX filename;
-vector<string> vlist;
-vector<string> vfilename;
+}//while
+inputTable.close();
 
-int countraw=0;
-int countcol=0;
-	//read until end
-int colnum=0;
-	while ((fscanf(sourcefp,"%s",info)) != EOF)
-	{
-		read = info;
-		delflag=1;
-		
-		countraw++;
-		colnum=0;
-		while ((pos = read.find(delimiter)) != string::npos) {
-    		token = read.substr(0, pos);
+//run into directory to figure out
 
-    		if(delflag>countcol){countcol=delflag;}
-    		list[countraw][colnum]=token;			
-    	
-    		colnum++;
-    		delflag++;
-    		read.erase(0, pos + delimiter.length());
-		}//while
-	}//while
-
-////close file
-CloseFile(&sourcefp);	
-
-
-
+//***********************
 //get file matrix
+//***********************
 DIR *dir;
 struct dirent *ent;
-int filenamenum=0;
-
 char const* ca = dirname.c_str();
+vector <vector <string> > Filedata;
 if ((dir = opendir (ca)) != NULL) {
   /* print all the files and directories within directory */
-  while ((ent = readdir (dir)) != NULL) {
- 
-   string filenameDIR=dirname+"/"+ent->d_name;
-   filename[filenamenum][0]=filenameDIR;
-   filename[filenamenum][2]=ent->d_name;
-   
-   vfilename.push_back(filenameDIR);
-  
-   char const* fica = filenameDIR.c_str();
-
-	size_t size = getFilesize(fica);
-
-  int convertdata = static_cast<int>(size);
-	//string filesize = to_string(convertdata);
-	//string filesize = lexical_cast<string>(convertdata);
-	ostringstream ostr;
-	ostr << convertdata;
-	string filesize = ostr.str();
-
-	filename[filenamenum][1]=filesize;
-	filenamenum++;
-
-  }
+	while ((ent = readdir (dir)) != NULL) {
+ 		vector <string> record;
+		string filenameDIR=dirname+"/"+ent->d_name;
+		string file = ent->d_name;
+		if(file.substr(0,1)!="."){
+			record.push_back(ent->d_name);
+			char const* fica = filenameDIR.c_str();
+			size_t size = getFilesize(fica);	
+			int convertdata = static_cast<int>(size);
+			ostringstream ostr;
+			ostr << convertdata;
+			string filesize = ostr.str();
+			record.push_back(filesize);
+			Filedata.push_back(record);
+		}
+  }//while
   closedir (dir);
 } else {
   /* could not open directory */
   perror ("");
   return EXIT_FAILURE;
 }
+//printMatrix(data);
 
-
-for (int i=0; i<1181;i++){
-	if (filename[i][1]=="0"){
-		for(int j=0;j<1065;j++){
-			if(list[j][1]+".gbk.16S.fa"==filename[i][2]||list[j][2]+".gbk.16S.fa"==filename[i][2]||list[j][3]+".gbk.16S.fa"==filename[i][2]){
-				string newfilename1 = newdirname1 + filename[i][2];
-				string newfilename2 = newdirname2 + filename[i][2];
-				char const* oname = filename[i][0].c_str();
-				char const* nname1 = newfilename1.c_str();
-				char const* nname2 = newfilename2.c_str();
-				
-				if(list[j][2]==""){
-					cout<<"alone-> case2 "+filename[i][2]<<endl<<flush;
-					rename(oname,nname2);
-				
-				}else{
-					cout<<"case1 "+filename[i][2]<<endl<<flush;
-					rename(oname,nname1);
+//***********************
+// compare
+//***********************
+for(int i=1;i<data.size();i++){
+	for(int j=1;j<data[i].size();j++){
+		//search file size
+		if(searchSize(data[i][j],Filedata)=="0"){
+			cout<<data[i][j]<<endl<<flush;
+			//go throurth row
+			int flag=0;
+			for (int l=1; l<data[i].size();l++){
+				//search file size
+				if(searchSize(data[i][l],Filedata)!="0" && searchSize(data[i][l],Filedata)!=""){
+					flag=1;
 				}
-			}
-		}
-
-	}
-}
-
-   
+			}//for
+			string oldFilename = dirname + "/" + data[i][j] + ".gbk.16S.fa";
+			char const* oname = oldFilename.c_str();
+			if(flag==1){
+				cout<<"case1"<<endl<<flush;
+				checkDir (newdirname1);
+				string newfilename1 = newdirname1 + "/" + data[i][j] + ".gbk.16S.fa";
+				char const* nname1 = newfilename1.c_str();
+				rename(oname,nname1);
+			}else{
+				cout<<"case2"<<endl<<flush;
+				checkDir (newdirname2);
+				string newfilename2 = newdirname2 + "/" + data[i][j] + ".gbk.16S.fa";
+				char const* nname2 = newfilename2.c_str();
+				rename(oname,nname2);
+				
+			}//if
+		}else if(searchSize(data[i][j],Filedata)==""){
+			//cout<<"no file"<<endl<<flush;
+		}else{
+			//cout<<"looks good"<<endl<<flush;
+		}//if search file size
+	}//for j
+}//for i
+	
 return 0;
 }
 //this is end of main
@@ -154,22 +149,45 @@ size_t getFilesize(const std::string& filename) {
     return st.st_size;   
 }
 
-// open file for reading
-int OpenFile(FILE **filePoint, string fileInput)
+// check openfile
+int checkFile(ifstream &input)
 {
-    const char *file = fileInput.c_str();
-    *filePoint = fopen (file,"r");
-    
-    if (filePoint == NULL){
-    	printf ("File could not be opened\n");
-    }else{
-    	printf ("File opened\n");
-    }
-	return 0;
+	if(input.fail()){                           //    Check open
+       cerr << "Can't open file\n";
+       exit(EXIT_FAILURE);
+       //return 1;
+	}else{return 0;}
 }
 
-// close file
-void CloseFile(FILE **filePoint)
-{
-	fclose(*filePoint); 	
+string searchSize(string &st,vector <vector <string> > &da){
+	string size="";
+	for (int k=0;k<da.size();k++){
+		if(st+".gbk.16S.fa"==da[k][0]){
+			size = da[k][1];
+		}
+	}
+	return size;
+}
+
+void printMatrix(vector <vector <string> > &dad){
+	for (int i=0;i<dad.size();i++){
+		for (int j=0;j<dad[i].size();j++){
+			cout<<dad[i][j]+" "<<flush;
+		}
+	cout<<endl<<flush;
+	}
+}
+
+void checkDir (string &direc){
+	DIR *dir;
+	struct dirent *ent;
+	char const* ca = direc.c_str();
+	if ((dir = opendir (ca)) != NULL) {
+		closedir (dir);
+	} else {
+  		/* could not open directory */
+  		string mkdir = "mkdir "+direc;
+  		char const* run = mkdir.c_str();
+  		system(run);
+	}
 }
