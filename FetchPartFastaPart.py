6d0c8999acb0aad0fa86e3676715558dd076734f
Jin Choi <jinchoi721@gmail.com>
2015-04-15 11:18:29 -0400

    update

---
+++ FetchPartFastaPart.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+import urllib2
+import os
+import sys
+import time
+
+if len(sys.argv) != 3:
+    print "USAGE: fetch_genome.py <genome_id_list> <out_dir>"
+    sys.exit(1)
+
+url_template = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id={0}&seq_start={1}&seq_stop={2}&rettype=fasta&retmode=text"
+
+os.mkdir(sys.argv[2])
+
+for id in open(sys.argv[1]):
+    id = id.strip()
+    line = id
+    if id == "":
+        continue
+
+    words = line.split(',')
+    id = words[0]
+    s1 = url_template.format(words[0],words[1],words[2])
+
+    sys.stdout.write("Fetching %s..." % id)
+    sys.stdout.flush()
+    gbk_out_file = os.path.join(sys.argv[2], id + ".fa")
+    if os.path.exists(gbk_out_file):
+        print "already fetched"
+
+    open(gbk_out_file, "w").write(urllib2.urlopen(s1).read())
+    print "Done"
+    time.sleep(1.0/3)
+
