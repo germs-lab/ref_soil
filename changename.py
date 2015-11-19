03225014eed6ce3881c1ba46bce545431f8ed6b7
Jin Choi <jinchoi721@gmail.com>
2015-11-19 16:20:33 -0600

    first commit

---
+++ changename.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# python changename.py input
+import sys
+fread = open(sys.argv[1],'r')
+
+temp = []
+for line in fread:
+    temp.append(line.strip())
+
+fread.close()
+fwrite = open(sys.argv[1],'w')
+for i in range(len(temp)):
+    if(temp[i][:1]=='>'):
+        filename = sys.argv[1].split('/')
+        fwrite.write('>'+filename[len(filename)-1]+' '+temp[i][1:]+' 16S rRNA gene'+'\n')
+    else :
+        fwrite.write(temp[i]+'\n')
