20196035a51248683ea27ea70ff7d69d897cfe1b
Jin Choi <jinchoi721@gmail.com>
2015-11-20 13:17:40 -0600

    update

---
+++ TreeAddTax.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# This script make annotation file
+#python TreeAddTax.py RefSoilFullTax_w_id.txt Annot.step2.txt
+import sys, os
+import modules
+
+full_path = os.path.realpath(__file__)
+filedefault = os.path.dirname(full_path)+"/DefaultAnnoNoRing.txt"
+deread = open(filedefault,'r')
+fwrite = open(sys.argv[2],'w')
+
+#write default
+for line in deread:
+    fwrite.write(line)
+fwrite.write('\n')
+
+#Make Tax table
+Tax = modules.ReadRefSoilTax(sys.argv[1])
+
+# class color assignment                                                              
+classColor = modules.AssignColor2(Tax)
+for i in range(len(Tax)):
+    tempColor = "k"
+    for j in range(len(classColor)):
+        if (classColor[j][0]==Tax[i][2]):
+            tempColor = classColor[j][1]
+    fwrite.write(Tax[i][2]+'\t'+"annotation"+'\t'+tempColor+'\n')
+    fwrite.write(Tax[i][2]+'\t'+"annotation_background_color"+'\t'+tempColor+'\n')
+    fwrite.write(Tax[i][0]+'\t'+"clade_marker_color"+'\t'+tempColor+'\n')
+    fwrite.write(Tax[i][0]+'\t'+"clade_marker_size"+'\t'+"30"+'\n')
+    fwrite.write(Tax[i][0]+'\t'+"clade_marker_edge_width"+'\t'+"0.1"+'\n')
