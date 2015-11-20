9f438ef7fc0444755d4903a33cc6c840ebaf9887
Jin Choi <jinchoi721@gmail.com>
2015-11-20 11:13:18 -0600

    update

--- GenbankToTax.py
+++ GenbankToTax.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
-# python GenbankToTax.py input.gbk
+# python GenbankToTax.py input.gbk output.txt
 
 import sys
 
 fread = open(sys.argv[1])
-
+fwrite = open(sys.argv[2])
 filename = sys.argv[1].split('/')
-print filename[len(filename)-1][:-3]
+fwrite.write(filename[len(filename)-1][:-4]+'\t')
 
 for line in fread:
     if ("taxon:" in line):
         tempcol = line.strip().split(":")
-        print tempcol[1][:-1]
+        fwrite.write(tempcol[1][:-1]+'\n')
         break
