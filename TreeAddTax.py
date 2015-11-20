d2be25ee3f8a4ecabc00291264a06ff80bec34af
Jin Choi <jinchoi721@gmail.com>
2015-11-20 14:03:03 -0600

    update

--- TreeAddTax.py
+++ TreeAddTax.py
@@ -25,8 +25,8 @@ for i in range(len(Tax)):
         if (classColor[j][0]==Tax[i][2]):
             tempColor = classColor[j][1]
     NoDocTax = Tax[i][0].split('.')
-    fwrite.write(NoDocTax[0]+'\t'+"annotation"+'\t'+Tax[i][2]+'\n')
-    fwrite.write(NoDocTax[0]+'\t'+"annotation_background_color"+'\t'+tempColor+'\n')
+    #fwrite.write(NoDocTax[0]+'\t'+"annotation"+'\t'+Tax[i][2]+'\n')
+    #fwrite.write(NoDocTax[0]+'\t'+"annotation_background_color"+'\t'+tempColor+'\n')
     fwrite.write(NoDocTax[0]+'\t'+"clade_marker_color"+'\t'+tempColor+'\n')
     fwrite.write(NoDocTax[0]+'\t'+"clade_marker_size"+'\t'+"30"+'\n')
     fwrite.write(NoDocTax[0]+'\t'+"clade_marker_edge_width"+'\t'+"0.1"+'\n')
