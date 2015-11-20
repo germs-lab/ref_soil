dbb0f5f3db802a9352a04888158a05d6d930ca9a
Jin Choi <jinchoi721@gmail.com>
2015-11-20 14:24:08 -0600

    update

--- changename.py
+++ changename.py
@@ -13,6 +13,6 @@ for i in range(len(temp)):
     if(temp[i][:1]=='>'):
         filename = sys.argv[1].split('/')
         thisname = filename[len(filename)-1][:-2].split('.')
-        fwrite.write('>'+thisname+' '+temp[i][1:]+' 16S rRNA gene'+'\n')
+        fwrite.write('>'+thisname[0]+' '+temp[i][1:]+' 16S rRNA gene'+'\n')
     else :
         fwrite.write(temp[i]+'\n')
