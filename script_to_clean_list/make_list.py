4647c15063aeea76ad22a44872c4246d7312554b
root <root@hi-mem-doe-ii.novalocal>
2016-01-29 23:01:32 +0000

    update make_list

--- script_to_clean_list/make_list.py
+++ script_to_clean_list/make_list.py
@@ -4,7 +4,7 @@ import genbank_get
 import taxonomy_finder
 
 fread = open(sys.argv[1],'r')
-
+path = "../../refsoilgenbank_v1_29_2016/"
 for line in fread:
     splt = line.strip().split(',')
     ld = ""
@@ -14,5 +14,7 @@ for line in fread:
     ft = ""
     ld = splt[0]
     ac = line.strip()
-    
+    ti = genbank_get.taxon(path+splt[0]+'.gbk')
+    name = genbank_get.organism(path+splt[0]+'.gbk')
+    ft = taxonomy_finder.full(path+splt[0]+'.gbk')
     print ld + '\t' + ac + '\t' + ti + '\t' + name + '\t' + ft
