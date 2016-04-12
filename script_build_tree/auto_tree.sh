c99548aea1c6b9aa516e6a5a7fc4d8f4a72daa3f
Jin Choi <jinchoi721@gmail.com>
2016-04-12 14:10:34 -0500

    add auto

---
+++ script_build_tree/auto_tree.sh
@@ -0,0 +1,10 @@
+python make_anno_refsoil.py ../RefSoil_v1.txt.split.bacteria.txt anno.step2.txt
+graphlan_annotate.py --annot anno.step2.txt RefSoil_bac_ali.tree step2.xml
+python add_kingdom.py ../RefSoil_v1.txt.split.bacteria.txt anno.step3.txt
+graphlan_annotate.py --annot anno.step3.txt step2.xml step3.xml
+python add_legend.py ../RefSoil_v1.txt.split.bacteria.txt anno.step4.txt
+graphlan_annotate.py --annot anno.step4.txt step3.xml step4.xml
+python make_abun_table.py soil_type_count.txt refsoil_emp_blast.out.txt > soil_type_abun.txt
+python add_ring_soil_type.py soil_type_abun.txt anno.step5.txt
+graphlan_annotate.py --annot anno.step5.txt step4.xml step5.xml
+graphlan.py step5.xml step5.png --dpi 300 --size 15 --pad 0.6 --external_legends
