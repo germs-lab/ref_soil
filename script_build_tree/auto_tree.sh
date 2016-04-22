6960ad7bca4878b10d2221b6d77a6aa4b98da132
Jin Choi <jinchoi721@gmail.com>
2016-04-22 08:22:01 -0500

    update

--- script_build_tree/auto_tree.sh
+++ script_build_tree/auto_tree.sh
@@ -4,7 +4,4 @@ python add_kingdom.py bacteria.txt anno.step3.txt
 graphlan_annotate.py --annot anno.step3.txt step2.xml step3.xml
 python add_legend.py bacteria.txt anno.step4.txt
 graphlan_annotate.py --annot anno.step4.txt step3.xml step4.xml
-python make_abun_table.py soil_type_count.txt refsoil_emp_blast.out.txt > soil_type_abun.txt
-python add_ring_soil_type.py soil_type_abun.txt anno.step5.txt
-graphlan_annotate.py --annot anno.step5.txt step4.xml step5.xml
-graphlan.py step5.xml step5.png --dpi 300 --size 15 --pad 0.6 --external_legends
+graphlan.py step4.xml step4.png --dpi 300 --size 15 --pad 0.6 --external_legends
