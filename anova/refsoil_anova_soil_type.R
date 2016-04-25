d4d483bc2b00cdc03ca4d6de1969db2f3b7d9a69
Jin Choi <jinchoi721@gmail.com>
2016-04-25 16:21:58 -0500

    update

--- anova/refsoil_anova_soil_type.R
+++ anova/refsoil_anova_soil_type.R
@@ -1,8 +1,8 @@
-setwd=("~/Documents/Research/2016/4April/anova/")
-data <- read.table("~/Documents/Research/2016/4April/anova/anova_input.txt",header=TRUE)
-
+#setwd=("~/Documents/Research/2016/4April/anova/")
+#data <- read.table("~/Documents/Research/2016/4April/anova/anova_input.txt",header=TRUE)
+data <- read.table("anova_input.txt",header=TRUE)
 colnames(data) <- c("otu","type","value")
-sum = summarySE(data, measurevar="value", groupvars=c("otu","type"))
+#sum = summarySE(data, measurevar="value", groupvars=c("otu","type"))
 
 model = lm(value ~ otu + type + otu:type,data=data)
 anova(model)  
