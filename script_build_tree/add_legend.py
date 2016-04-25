6fd2ddbd3f690da5ecee89c253d8e5fa45fb816e
Jin Choi <jinchoi721@gmail.com>
2016-04-25 07:50:28 -0500

    update;

--- script_build_tree/add_legend.py
+++ script_build_tree/add_legend.py
@@ -20,7 +20,7 @@ KingdomColor = [["Bacteria",'#EE6A50'],["Archaea",'#9ACD32']]
 
 #print legend
 for i in classColor:
-    fwrite.write(i[0]+'\t'+"annotation"+'\t'+i[0]+'\n')
+    fwrite.write(i[0]+'\t'+"annotation"+'\t'+"*:"+i[0]+'\n')
     fwrite.write(i[0]+'\t'+"annotation_background_color"+'\t'+i[1]+'\n')
     fwrite.write(i[0]+'\t'+"clade_marker_color"+'\t'+i[1]+'\n')
     fwrite.write(i[0]+'\t'+"annotation_legend_font_size"+'\t'+"2"+'\n')
