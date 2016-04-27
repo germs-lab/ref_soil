69664fa978d50f7b1a9bb8e3a04d292b85e0320f
Jin Choi <jinchoi721@gmail.com>
2016-04-27 16:02:40 -0500

    update

--- script_gap/count_tax_rdp.py
+++ script_gap/count_tax_rdp.py
@@ -4,5 +4,14 @@
 
 import sys
 import read_rdp_taxonomy
-dict = read_rdp_taxonomy.get_tax_dict(sys.argv[1])
-print len(dict)
+dict = {}
+for line in open(sys.argv[1],'r'):
+    tax = read_rdp_taxonomy.get_tax_dict(line)
+    if dict.has_key(tax[2]):
+        temp = dict[tax[2]]
+        dict[tax[2]] = temp + 1
+    else:
+        dict[tax[2]] = 1
+
+for item in dict.items():
+    print item[0],item[1]
